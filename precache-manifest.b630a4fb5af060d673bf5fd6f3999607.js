0314b02032ec28ec5e438fc24e4722e2fadfa94c
Avi Jain <avijain1001@gmail.com>
2020-06-12 01:41:26 +0530

    Updates

---
+++ precache-manifest.b630a4fb5af060d673bf5fd6f3999607.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdda1889bd3da19db064f74553adc56b",
+    "url": "/avijain10/avijain10.github.io/index.html"
+  },
+  {
+    "revision": "7263f8cf1b6bc11bd12d",
+    "url": "/avijain10/avijain10.github.io/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bd1f0102edaa3283f280",
+    "url": "/avijain10/avijain10.github.io/static/css/main.ba00ef65.chunk.css"
+  },
+  {
+    "revision": "7263f8cf1b6bc11bd12d",
+    "url": "/avijain10/avijain10.github.io/static/js/2.93341508.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/avijain10/avijain10.github.io/static/js/2.93341508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1f0102edaa3283f280",
+    "url": "/avijain10/avijain10.github.io/static/js/main.3ec01a9c.chunk.js"
+  },
+  {
+    "revision": "cba5ec3e429a4acfc000",
+    "url": "/avijain10/avijain10.github.io/static/js/runtime-main.100578e0.js"
+  },
+  {
+    "revision": "585122417e44ecc7f63dc407c97fde33",
+    "url": "/avijain10/avijain10.github.io/static/media/homepage1.58512241.jpg"
+  },
+  {
+    "revision": "4d684da5f2de3a288b861aafeec78ba7",
+    "url": "/avijain10/avijain10.github.io/static/media/kohi.4d684da5.jpg"
+  },
+  {
+    "revision": "6af72b5b20805783c9625df3ce126c7e",
+    "url": "/avijain10/avijain10.github.io/static/media/mars.6af72b5b.jpg"
+  },
+  {
+    "revision": "c1b8bb9b75228fa76cdde6e2efcb2c9a",
+    "url": "/avijain10/avijain10.github.io/static/media/symbi.c1b8bb9b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.d980a90cd084b430936dcd3ab03f5b6e.js"
+  "/avijain10/avijain10.github.io/precache-manifest.b630a4fb5af060d673bf5fd6f3999607.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avijain10/avijain10.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
