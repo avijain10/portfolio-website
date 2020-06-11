ddc0f2eaba18b124cc23d79e2af63007a2138673
Avi Jain <avijain1001@gmail.com>
2020-06-11 22:52:32 +0530

    Updates

---
+++ precache-manifest.48f36b3608ca5b425f25def8f7a88a99.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2b0ec581afd3c51ec2741b7dfa50c6",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "7263f8cf1b6bc11bd12d",
+    "url": "/portfolio-website/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1debf2d36aabed83e4ae",
+    "url": "/portfolio-website/static/css/main.ba00ef65.chunk.css"
+  },
+  {
+    "revision": "7263f8cf1b6bc11bd12d",
+    "url": "/portfolio-website/static/js/2.93341508.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-website/static/js/2.93341508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1debf2d36aabed83e4ae",
+    "url": "/portfolio-website/static/js/main.695f16a1.chunk.js"
+  },
+  {
+    "revision": "c3fb84e40b4140417d62",
+    "url": "/portfolio-website/static/js/runtime-main.3f55a665.js"
+  },
+  {
+    "revision": "585122417e44ecc7f63dc407c97fde33",
+    "url": "/portfolio-website/static/media/homepage1.58512241.jpg"
+  },
+  {
+    "revision": "4d684da5f2de3a288b861aafeec78ba7",
+    "url": "/portfolio-website/static/media/kohi.4d684da5.jpg"
+  },
+  {
+    "revision": "6af72b5b20805783c9625df3ce126c7e",
+    "url": "/portfolio-website/static/media/mars.6af72b5b.jpg"
+  },
+  {
+    "revision": "c1b8bb9b75228fa76cdde6e2efcb2c9a",
+    "url": "/portfolio-website/static/media/symbi.c1b8bb9b.jpg"
+  }
+]);
